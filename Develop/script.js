1f4b3fe9e01bd8d2d5351b0f211f9750d5695db4
Christina Brenia <ChristinaBrenia@Christinas-MBP.home>
2020-10-31 00:17:09 -0400

    sync number of character scroller in javascript

--- Develop/script.js
+++ Develop/script.js
@@ -5,19 +5,26 @@ const characterAmountRange = document.getElementById
 const characterAmountNumber = document.getElementById
 ('characterAmountNumber')
 
-characterAmountNumber.addEventListener('input','syncCharacterAmount')
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+characterAmountRange.addEventListener('input', syncCharacterAmount)
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+function syncCharacterAmount(e) {
+  const value = e.target.value
+  characterAmountNumber.value = value
+  characterAmountRange.value = value
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
